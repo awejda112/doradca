96fd11a3db82ae4bea388a8c721def6be2917745
awejda112 <awejda112@proton.me>
2025-01-09 18:41:09 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,32 +1,29 @@
 <?php
-// Sprawdzenie, czy formularz został wysłany metodą POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Odbieranie danych z formularza
-    $name = htmlspecialchars($_POST['name']);  // Imię i nazwisko
-    $email = htmlspecialchars($_POST['email']);  // E-mail
-    $message = htmlspecialchars($_POST['message']);  // Wiadomość
+    // Dane z formularza
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
-    // Adres e-mail, na który mają być wysyłane wiadomości
-    $to = "awejda112@proton.me";  // Twój adres e-mail
-
-    // Temat wiadomości
-    $subject = "Wiadomość z formularza kontaktowego";
-
+    // Adres e-mail odbiorcy
+    $to = "awejda112@proton.me"; // Twój adres e-mail
+    $subject = "Nowa wiadomość od $name";
+    
     // Treść wiadomości
-    $body = "Imię i nazwisko: $name\nE-mail: $email\nWiadomość: $message";
+    $messageBody = "Imię i nazwisko: $name\n";
+    $messageBody .= "E-mail: $email\n\n";
+    $messageBody .= "Wiadomość:\n$message";
 
     // Nagłówki wiadomości
-    $headers = "From: $email" . "\r\n" .
-               "Reply-To: $email" . "\r\n" .
-               "Content-Type: text/plain; charset=UTF-8";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    // Funkcja mail do wysyłania wiadomości
-    if (mail($to, $subject, $body, $headers)) {
-        // Jeśli wiadomość została wysłana
-        echo "Wiadomość została wysłana.";
+    // Wysyłanie wiadomości
+    if (mail($to, $subject, $messageBody, $headers)) {
+        echo "Wiadomość została wysłana!";
     } else {
-        // Jeśli wystąpił błąd podczas wysyłania
-        echo "Wystąpił błąd podczas wysyłania wiadomości.";
+        echo "Wystąpił problem podczas wysyłania wiadomości.";
     }
 }
 ?>
